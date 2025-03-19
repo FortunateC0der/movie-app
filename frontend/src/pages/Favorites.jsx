714d8ff5aa221e77baeda93685cb6fd9595579af
FortunateC0der <fortunate.mothoana@gmail.com>
2025-03-18 18:14:07 -0700

    add routes

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,3 +1,10 @@
-function Favorite() {
-  return <div></div>;
+function Favorites() {
+  return (
+    <div className="favorites-empty">
+      <h2>No favorite movie yet</h2>
+      <p>Start by adding yor first movie to your favorites</p>
+    </div>
+  );
 }
+
+export default Favorites;
